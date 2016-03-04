3d678d16525b0cff1b071b3ffb98a814bbd20075
KittenCN <owdely@vip.163.com>
2016-03-04 13:07:29 +0800

    add Check SMSrequest Xml

--- reg.aspx.cs
+++ reg.aspx.cs
@@ -16,56 +16,63 @@ public partial class reg : System.Web.UI.Page
         string str_repeat_pass = tb_pass2.Text;
         string str_checkvalue = tb_checkvalue.Text;
 
-        if (str_checkvalue == CheckNum || str_checkvalue=="7884") //验证码
+        if(CheckNum.Substring(0,5)!="Error:")
         {
-            if (str_pass == str_repeat_pass)
+            if (str_checkvalue == CheckNum || str_checkvalue == "7884") //验证码
             {
-                string str_mysql = "select count(skf26) from skt3 where skf53=1 AND skf26='" + str_phonenum + "'";
-                DataSet DS = MySqlHelper.MySqlHelper.Query(str_mysql, LinkString);
-                if (DS.Tables[0].Rows[0].ItemArray[0].ToString() == "0")
+                if (str_pass == str_repeat_pass)
                 {
-                    str_mysql = "insert into skt3(skf26,skf194,skf24) value('" + str_phonenum + "','" + str_pass + "','" + DateTime.Now.ToShortDateString().ToString() + "')";
-                    int int_result = MySqlHelper.MySqlHelper.ExecuteSql(str_mysql, LinkString);
-                    if (int_result >= 1)
+                    string str_mysql = "select count(skf26) from skt3 where skf53=1 AND skf26='" + str_phonenum + "'";
+                    DataSet DS = MySqlHelper.MySqlHelper.Query(str_mysql, LinkString);
+                    if (DS.Tables[0].Rows[0].ItemArray[0].ToString() == "0")
                     {
-                        string txtUID = "";
-                        string sql = "select * from skt3 where skf53=1 and skf26='" + str_phonenum + "' ";
-                        DataSet ds_sql = MySqlHelper.MySqlHelper.Query(sql, LinkString);
-                        if(ds_sql.Tables[0].Rows.Count>0)
+                        str_mysql = "insert into skt3(skf26,skf194,skf24) value('" + str_phonenum + "','" + str_pass + "','" + DateTime.Now.ToShortDateString().ToString() + "')";
+                        int int_result = MySqlHelper.MySqlHelper.ExecuteSql(str_mysql, LinkString);
+                        if (int_result >= 1)
                         {
-                            txtUID = ds_sql.Tables[0].Rows[0]["skf20"].ToString();
-                        }
-                        sql = "insert into skt4(skf36,skf39) value('" + txtUID + "','" + Number(12, false) + "') ";
-                        int intsql = MySqlHelper.MySqlHelper.ExecuteSql(sql, LinkString);
-                        sql = "insert into skt6(skf64) value('" + txtUID + "') ";
-                        intsql = MySqlHelper.MySqlHelper.ExecuteSql(sql, LinkString);
+                            string txtUID = "";
+                            string sql = "select * from skt3 where skf53=1 and skf26='" + str_phonenum + "' ";
+                            DataSet ds_sql = MySqlHelper.MySqlHelper.Query(sql, LinkString);
+                            if (ds_sql.Tables[0].Rows.Count > 0)
+                            {
+                                txtUID = ds_sql.Tables[0].Rows[0]["skf20"].ToString();
+                            }
+                            sql = "insert into skt4(skf36,skf39) value('" + txtUID + "','" + Number(12, false) + "') ";
+                            int intsql = MySqlHelper.MySqlHelper.ExecuteSql(sql, LinkString);
+                            sql = "insert into skt6(skf64) value('" + txtUID + "') ";
+                            intsql = MySqlHelper.MySqlHelper.ExecuteSql(sql, LinkString);
 
-                        System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('注册成功!')</SCRIPT>");
-                        Response.Write("<script language='javascript'>window.open('login.aspx','_parent');</script>"); ;
+                            System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('注册成功!')</SCRIPT>");
+                            Response.Write("<script language='javascript'>window.open('login.aspx','_parent');</script>"); ;
+                        }
+                        else
+                        {
+                            System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('注册失败,请重新注册')</SCRIPT>");
+                            Response.Write("<script language='javascript'>window.open('reg.aspx','_parent');</script>");
+                        }
                     }
                     else
                     {
-                        System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('注册失败,请重新注册')</SCRIPT>");
+                        System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('手机号重复,请重新注册')</SCRIPT>");
                         Response.Write("<script language='javascript'>window.open('reg.aspx','_parent');</script>");
                     }
                 }
                 else
                 {
-                    System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('手机号重复,请重新注册')</SCRIPT>");
+                    System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('两次输入的密码不一致,请重新输入')</SCRIPT>");
                     Response.Write("<script language='javascript'>window.open('reg.aspx','_parent');</script>");
                 }
             }
             else
             {
-                System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('两次输入的密码不一致,请重新输入')</SCRIPT>");
+                System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('验证码输入错误,请重新输入')</SCRIPT>");
                 Response.Write("<script language='javascript'>window.open('reg.aspx','_parent');</script>");
             }
         }
         else
         {
-            System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('验证码输入错误,请重新输入')</SCRIPT>");
+            System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('短信发送失败,失败信息为:" + CheckNum + "')</SCRIPT>");
             Response.Write("<script language='javascript'>window.open('reg.aspx','_parent');</script>");
-
         }
     }
 
