abb49b6fbb476548dd580a8f63a1812d44b56c72
KittenCN <owdely@vip.163.com>
2016-04-11 16:19:53 +0800

    edit data source of the point and money form

--- point.aspx.cs
+++ point.aspx.cs
@@ -12,12 +12,17 @@ public partial class point : System.Web.UI.Page
         {
             string str_userid = Session["UID"].ToString();
 
-            string str_mysql = "SELECT skf77-skf76 AS '增加储值', skf79-skf78 AS '增加积分', skf81-skf80 AS '增加赠送积分',skf82 AS '生成时间' FROM skt7 WHERE skf73='" + str_userid + "'";
+            //string str_mysql = "SELECT skf77-skf76 AS '增加储值', skf79-skf78 AS '增加哈宝币', skf81-skf80 AS '增加赠送哈宝币',skf82 AS '生成时间' FROM skt7 WHERE skf73='" + str_userid + "'";
+            string str_mysql = "SELECT skf79-skf78 AS '增加哈宝币', skf81-skf80 AS '增加赠送哈宝币',skf82 AS '生成时间' FROM skt7 WHERE (skf79-skf78<>0 or skf81-skf80<>0) and skf73='" + str_userid + "'";
             DataSet DS = MySqlHelper.MySqlHelper.Query(str_mysql, LinkString);
             if (DS.Tables[0].Rows.Count > 0 && DS.Tables[0].Rows[0].ItemArray[0].ToString() != null)
             {
                 gv_hbb.DataSource = DS;
                 gv_hbb.DataBind();
+            }
+            else
+            {
+                System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('没有查询到数据!')</SCRIPT>");
             }
 
             str_mysql = "select skf26 from skt3 where skf53=1 and skf20='" + Session["UID"].ToString() + "'";
