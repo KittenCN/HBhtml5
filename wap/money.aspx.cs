bc88255161acda574188478c363515bc083b064c
KittenCN <owdely@vip.163.com>
2016-06-13 13:04:53 +0800

    limit showing four records in all money or point forms

--- wap/money.aspx.cs
+++ wap/money.aspx.cs
@@ -18,7 +18,7 @@ public partial class money : System.Web.UI.Page
             //str_mysql = str_mysql + " AND(skf158 IN(SELECT skf37 FROM skt4 WHERE skf36 = '" + str_userid + "' AND skf54 = 1) ";
             //str_mysql = str_mysql + " OR skf158 IN(SELECT skf38 FROM skt4 WHERE skf36 = '" + str_userid + "' AND skf54 = 1) ";
             //str_mysql = str_mysql + " OR skf158 IN(SELECT skf39 FROM skt4 WHERE skf36 = '" + str_userid + "' AND skf54 = 1)) ";
-            string str_mysql = "SELECT skf77-skf76 AS '增加储值',skf82 AS '生成时间' FROM skt7 WHERE skf77-skf76<>0 and skf73='" + str_userid + "'";
+            string str_mysql = "SELECT skf77-skf76 AS '增加储值',skf82 AS '生成时间' FROM skt7 WHERE skf77-skf76<>0 and skf73='" + str_userid + "' ORDER BY skf82 DESC LIMIT 0,20";
             DataSet DS = MySqlHelper.MySqlHelper.Query(str_mysql, LinkString);
             if (DS.Tables[0].Rows.Count > 0 && DS.Tables[0].Rows[0].ItemArray[0].ToString() != null)
             {
--- wap/point.aspx.cs
+++ wap/point.aspx.cs
@@ -13,7 +13,7 @@ public partial class point : System.Web.UI.Page
             string str_userid = Session["UID"].ToString();
 
             //string str_mysql = "SELECT skf77-skf76 AS '增加储值', skf79-skf78 AS '增加哈宝币', skf81-skf80 AS '增加赠送哈宝币',skf82 AS '生成时间' FROM skt7 WHERE skf73='" + str_userid + "'";
-            string str_mysql = "SELECT skf79-skf78 AS '增加哈宝币', skf81-skf80 AS '增加赠送哈宝币',skf82 AS '生成时间' FROM skt7 WHERE (skf79-skf78<>0 or skf81-skf80<>0) and skf73='" + str_userid + "'";
+            string str_mysql = "SELECT skf79-skf78 AS '增加哈宝币', skf81-skf80 AS '增加赠送哈宝币',skf82 AS '生成时间' FROM skt7 WHERE (skf79-skf78<>0 or skf81-skf80<>0) and skf73='" + str_userid + "' ORDER BY skf82 DESC LIMIT 0,20";
             DataSet DS = MySqlHelper.MySqlHelper.Query(str_mysql, LinkString);
             if (DS.Tables[0].Rows.Count > 0 && DS.Tables[0].Rows[0].ItemArray[0].ToString() != null)
             {
